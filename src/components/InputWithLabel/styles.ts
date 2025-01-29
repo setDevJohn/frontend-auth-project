b5eaca5b09b23988868a1aa292129e726c85ff6b
Jhony Freitas <jhony00._@hotmail.com>
2025-01-29 19:46:49 -0300

    styles: Update input style

--- src/components/InputWithLabel/styles.ts
+++ src/components/InputWithLabel/styles.ts
@@ -3,17 +3,20 @@ import styled from "styled-components";
 export const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 3px;
 `;
 export const InputLabel = styled.label`
+  font-family: "Poppins", serif;
   font-size: 1.4rem;
+  margin-bottom: -4px;
 `;
 export const InputEntrie = styled.input`
-  padding: 3px 7px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 1.5rem;
-  
+  color: #6d6b6b;
+  font-family: "Poppins", serif;
+  font-size: 1.4rem;
+  border: none;
+  border-bottom: 2px solid #9d9d9d;
+  border-radius: 3px;
+  padding: 5px 7px 4px 10px;
   
   &:focus {
     outline: none;
