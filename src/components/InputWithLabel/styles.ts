4c69ed7755ef10e341f263db53a36741e81c7977
Jhony Freitas <jhony00._@hotmail.com>
2025-02-18 23:58:54 -0300

    styles: Change input styles

--- src/components/InputWithLabel/styles.ts
+++ src/components/InputWithLabel/styles.ts
@@ -25,9 +25,9 @@ export const InputEntrie = styled.input<TInputEntrie>`
   color: #2f2f2f;
   font-family: "Poppins", serif;
   font-size: 1.3rem;
-  border: ${({ $error }) => `1px solid ${$error ? '#e23333' : '#cbcbcb'}`};
+  border: ${({ $error }) => `1px solid ${$error ? '#e23333' : '#eee'}`};
   border-radius: 4px;
-  box-shadow: 1px 2px 4px #0003;
+  box-shadow: 1px 2px 7px -2px #0004;
   padding: 5px 7px 4px 13px;
   width: 100%;
   max-width: 243px;
