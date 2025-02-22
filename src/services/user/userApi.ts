ef34141353e1fc1f3f202e237ada4da7b89dc409
Jhony Freitas <jhony00._@hotmail.com>
2025-02-22 11:02:27 -0300

    feat: Create token verification api

--- src/services/user/userApi.ts
+++ src/services/user/userApi.ts
@@ -18,6 +18,38 @@ async function register (userData: TCreateUserResquest) {
   }
 }
 
+async function resetPassword (email: string) {
+  try {
+    return await api.post('/user/resetPassword', { email });
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado.');
+  }
+}
+
+async function confirmToken (token: string) {
+  try {
+    return await api.post('/user/confirmToken', { token });
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado.');
+  }
+}
+
 export const userApi = {
   register,
+  resetPassword,
+  confirmToken,
 };
