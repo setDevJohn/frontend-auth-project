0d1f78b532a404c7e439ad8eabd5290349855a6d
Jhony Freitas <jhony00._@hotmail.com>
2025-02-22 11:02:10 -0300

    feat: Add Token verification

--- src/components/ResetPassword/index.tsx
+++ src/components/ResetPassword/index.tsx
@@ -1,9 +1,11 @@
 import { FormEvent, useState } from 'react';
-import { toastError, toastSuccess } from '@utils/toast';
+import { toastError } from '@utils/toast';
 import { InputWithLabel } from '@components/InputWithLabel';
 import { FormButton } from '@components/FormButton';
 import { Container, Form, FormContainer, RouteBack, Text, Title } from './styles';
 import { LoadingDots } from '@components/LoadingDots';
+import { userApi } from '@services/user/userApi';
+import { useNavigate } from 'react-router-dom';
 
 type TComponent = {
   resetModal: boolean; 
@@ -12,36 +14,32 @@ type TComponent = {
 
 export function ResetPassword ({ resetModal, setResetModal } : TComponent) {
   const [email, setEmail] = useState<string>('');
+  const [token, setToken] = useState<string>('');
   const [loading, setLoading] = useState(false);
   const [emailSent, setEmailSent] = useState(false);
 
-  function handleInputChange (target) {
-    const { name, value } = target;
-    if (name === 'email') {
-      return setEmail(value);
-    } 
-    // Criar mascara para token ? 
-    // Se não e essa função for desnecessária passar direto no settings
-  }
+  const navigate = useNavigate();
 
   async function handleSubmit (e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    
-    // const errorFields = Object.entries(loginForm).filter(field => !field[1]);
-    // if (errorFields.length) {
-    //   setErrorFields(errorFields.map(field => field[0]));
-    //   return toastWarn('Preencha todos os campos');
-    // }
+
+    if (!emailSent && !email) return toastError('E-mail é obrigatório');
+    if (emailSent && !token) return toastError('Token é obrigatório');
   
     try {
       setLoading(true);
-      // await authApi.login({ login: loginForm.login, pass: loginForm.pass });
-  
-      toastSuccess('Login Success');
-      // resetForm();
+      if (emailSent) {
+        await userApi.confirmToken(token);
+        navigate('/');
+      } else {
+        await userApi.resetPassword(email);
+        setEmailSent(true);
+      }
     } catch (err) {
-      console.error('Erro ao criar usuário:', err);
-      toastError(err || 'Erro ao criar usuário');
+      console.error(emailSent 
+        ? 'Erro ao confirmar o token:'
+        : 'Erro ao enviar o email:', err.message);
+      toastError(err.message);
     } finally {
       setLoading(false);
     }
@@ -53,7 +51,7 @@ export function ResetPassword ({ resetModal, setResetModal } : TComponent) {
     label: emailSent ? 'Token' : 'E-mail',
     error: emailSent ? false : false,
     value: emailSent ? '' : email,
-    handleChange: (target) => emailSent ? () => {} : setEmail(target.value),
+    handleChange: (target) => emailSent ? setToken(target.value) : setEmail(target.value),
     styles: { minWidth: '243px' },
   }; 
 
