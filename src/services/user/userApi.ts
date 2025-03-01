6a2c56e85cd79cef7edf16b628cbb7d4769170af
Jhony Freitas <jhony00._@hotmail.com>
2025-03-01 02:52:00 -0300

    feat: Finally users endpoint

--- src/services/user/userApi.ts
+++ src/services/user/userApi.ts
@@ -18,9 +18,25 @@ async function register (userData: TCreateUserResquest) {
   }
 }
 
-async function resetPassword (email: string) {
+async function fetch (id: number) {
   try {
-    return await api.post('/user/resetPassword', { email });
+    const { data: response } = await api.get(`/user/${id}`);
+    return response;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado');
+  }
+}
+
+async function sendResetToken (email: string) {
+  try {
+    return await api.post('/user/sendResetToken', { email });
   } catch (err) {
     if (err instanceof AxiosError) {
       if (err.response) {
@@ -33,9 +49,61 @@ async function resetPassword (email: string) {
   }
 }
 
-async function confirmToken (token: string) {
+async function sendConfirmToken (email: string) {
   try {
-    return await api.post('/user/confirmToken', { token });
+    return await api.post('/user/sendConfirmToken', { email });
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado.');
+  }
+}
+
+async function confirmToken (token: string, email: string) {
+  try {
+    const { data: response } = await api.post('/user/confirmToken',
+      { token, email }
+    );
+    return response;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado.');
+  }
+}
+
+async function confirmAccount (id: number, token: string) {
+  try {
+    const { data: response } = await api.post('/user/confirmAccount',
+      { id, token }
+    );
+    return response;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      if (err.response) {
+        console.error(err);
+        throw new Error(err.response.data.message);
+      }
+      throw new Error(handleAxiosError(err));
+    }
+    throw new Error('Ocorreu um erro inesperado.');
+  }
+}
+
+async function resetPassword (userId: number, password: string) {
+  try {
+    const { data: response } = await api.post('/user/resetPassword', { id: userId, password });
+    return response;
   } catch (err) {
     if (err instanceof AxiosError) {
       if (err.response) {
@@ -50,6 +118,10 @@ async function confirmToken (token: string) {
 
 export const userApi = {
   register,
-  resetPassword,
+  fetch,
+  sendResetToken,
+  sendConfirmToken,
   confirmToken,
+  confirmAccount,
+  resetPassword,
 };
