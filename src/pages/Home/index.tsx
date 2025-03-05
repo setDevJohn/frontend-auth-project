808e86da2901a4d8339675365e5a6be9380bd666
Jhony Freitas <jhony00._@hotmail.com>
2025-03-04 21:58:28 -0300

    feat: Add logout function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,36 @@
+import { AuthContext } from 'context/Auth/auth';
+import { useContext } from 'react';
+import { SlLogout } from 'react-icons/sl';
+
 export function Home () {
+  const { handleLogout } = useContext(AuthContext);
+
   return (
-    <div>
-      <h2> Página inicial </h2>
+    <div 
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '25px',
+        height: '100svh',
+      }}
+    >
+      <h2 style={{ fontSize: '1.8rem' }}> Página inicial </h2>
+
+      <div 
+        onClick={handleLogout}
+        style={{
+          display: 'flex',
+          alignItems:'center',
+          gap: '5px',
+          fontSize: '1.7rem',
+          cursor: 'pointer',
+        }}
+      >
+        <SlLogout size={25} />
+        Sair
+      </div>
     </div>
   );
 }
